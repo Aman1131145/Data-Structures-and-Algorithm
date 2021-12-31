cef9927bd7d7d4d2bb2f4ede22021a2627c7e490
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2021-12-31 10:25:12 +0530

    sum of first n numbers

---
+++ Recursion/SumOfFirstNNaturalNo.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class SumOfFirstNNaturalNo {
+    
+    public static int sum( int n){
+        if(n == 1){
+            return 1;
+        }
+        return n + sum(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sum(n));
+        sc.close();
+    }
+}
