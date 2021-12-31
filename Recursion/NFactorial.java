34e2ac7fee925e0bb8a309f1dd313fb2ea4100dd
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2021-12-31 10:24:51 +0530

    factorial of n

---
+++ Recursion/NFactorial.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class NFactorial {
+
+    public static int fact(int n){
+        if(n==0){
+            return 1;            
+        }
+        
+        int smallAns = fact(n-1);
+        return n * smallAns;
+        
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = fact(n);
+        System.out.println(ans);
+    }
+}
