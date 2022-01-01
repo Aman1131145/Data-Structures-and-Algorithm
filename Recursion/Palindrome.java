508f4f18b2a473aae2c7511a659efeca98dcba8b
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2022-01-01 14:19:07 +0530

    palindrome number

--- Recursion/Palindrome.java
+++ Recursion/Palindrome.java
@@ -5,27 +5,18 @@ import java.util.Scanner;
 public class Palindrome {
 
     public static boolean isStringPalindrome(String input){
-        int arr1[] = new int[input.length()];
-        int arr2[] = new int[input.length()];
-        for(int i = 0; i < input.length()-1; i++){
-            arr1[i] = input.charAt(i);
-            arr2[i] = input.charAt(input.length()-i);
-        }
-        
-        System.out.println(arr1);
-        System.out.println(arr2);
-        if(input.length() == 0){
-            return false;
-        }
-        if(arr1 == arr2){
+        if(input.length()<=1)
             return true;
-        }else{
-            return false;
-        } 
+        if(input.charAt(0)==input.charAt(input.length()-1)){
+            return isStringPalindrome(input.substring(1,input.length()-1));
+        }
+        else
+             return false;
     }
+    
     public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		String input = "rececar";
+		String input = "raceecar";
         System.out.println(Palindrome.isStringPalindrome(input));
         s.close();
 	}
