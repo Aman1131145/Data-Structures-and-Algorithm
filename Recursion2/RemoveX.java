0cb90384ecab7d64e07e2c9908fd608213aff36e
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2022-01-02 22:50:41 +0530

    Remove X

---
+++ Recursion2/RemoveX.java
@@ -0,0 +1,25 @@
+package Recursion2;
+
+import java.util.Scanner;
+
+public class RemoveX {
+
+    private static String removeX(String input) {
+        if(input.isEmpty()){
+            return input;
+        }
+        if(input.charAt(0) == 'x'){
+            input.replaceAll("x", "");
+            return input;
+        }
+        return input;
+    }    
+    
+    public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String input = s.nextLine();
+		System.out.println(RemoveX.removeX(input));
+        s.close();
+	}
+
+}
