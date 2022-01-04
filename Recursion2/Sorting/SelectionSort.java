b59b4e2682f7517bd7b1127e8c09cfde6b11c417
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2022-01-04 19:29:37 +0530

    Selection Sort

--- Recursion2/Sorting/SelectionSort.java
+++ Recursion2/Sorting/SelectionSort.java
@@ -6,13 +6,10 @@ public class SelectionSort {
 
     public static int[] selectionSort(int arr[]){
         int n = arr.length;
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < n; i++) {
             int key = arr[i];
             int j = i - 1;
  
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
