a1264135174e7deab3e4aa2d3acbd135258c0b1d
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2022-01-04 19:35:52 +0530

    Merge Sort

--- Recursion2/Sorting/MergeSort.java
+++ Recursion2/Sorting/MergeSort.java
@@ -5,46 +5,34 @@ import java.util.Scanner;
 public class MergeSort {
 
     public static void merge(int arr[] , int arr1[] , int arr2[]){
-        
-        
         int i=0,j=0,k=0;
         
         while(i<arr1.length && j<arr2.length){
-            
         	if(arr1[i]<=arr2[j]){
                 arr[k] = arr1[i];
                 k++;
                 i++;
             }    
-            
             else{
                 arr[k] = arr2[j];
                 k++;
                 j++;
-            }
-            
+            }   
         }
-        
         while(i<arr1.length){
             arr[k] = arr1[i];
             k++;
             i++;
         }
-        
         while(j<arr2.length){
             arr[k] = arr2[j];
             k++;
             j++;
         }
-        
-        
     }
-    
-    
 
 	public static void mergeSort(int[] input){
-		// Write your code here
-        
+		// Write your code here        
         if(input.length<=1){
             return;
         }
@@ -52,13 +40,11 @@ public class MergeSort {
         int mid = input.length/2;
         
         int arr1[] = new int[mid];
-        
         int arr2[] = new int[input.length-mid];
         
         for(int i=0;i<mid;i++){
             arr1[i] = input[i];
         }
-        
         for(int j=mid;j<input.length;j++){
             arr2[j-mid] = input[j];
         }
@@ -66,7 +52,6 @@ public class MergeSort {
         mergeSort(arr1);
         mergeSort(arr2);
         merge(input,arr1,arr2);
-        
 	}
 
     public static int[] takeInput() {
