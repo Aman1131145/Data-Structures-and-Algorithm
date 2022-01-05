90fa3009291ff9743c0d2e2766b7692f00e2ab07
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2022-01-05 19:06:37 +0530

    Polynomial Use Class

--- OOPs1/polynomialClass/PolynomialUse.java
+++ OOPs1/polynomialClass/PolynomialUse.java
@@ -8,21 +8,21 @@ public class PolynomialUse {
         p1.setCoefficient(2, 2);
         p1.print();
 
-        // Polynomial p2 = new Polynomial();
-        // p2.setCoefficient(0, 3);
-        // p2.setCoefficient(2, 2);
-        // p2.setCoefficient(1, 4);
-        // p2.setCoefficient(2, 5);
-        // p2.setCoefficient(6, 4);
-        // p2.print();
+        Polynomial p2 = new Polynomial();
+        p2.setCoefficient(0, 3);
+        p2.setCoefficient(2, 2);
+        p2.setCoefficient(1, 4);
+        p2.setCoefficient(2, 5);
+        p2.setCoefficient(6, 4);
+        p2.print();
 
-        // p1.add(p2);
-        // p1.print();
-        // p2.print();
+        p1.add(p2);
+        p1.print();
+        p2.print();
 
-        // p2.multiply(p1);
-        // p2.print();
-        // p1.print();
+        p2.multiply(p1);
+        p2.print();
+        p1.print();
 
         // Polynomial p3 = new Polynomial.add(p1, p2);
         // p3.print();
