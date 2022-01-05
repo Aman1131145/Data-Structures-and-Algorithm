5b9817fee71298ef1d99e47661edcf2574433785
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2022-01-05 12:39:42 +0530

    N Factorial

--- Recursion/NFactorial.java
+++ Recursion/NFactorial.java
@@ -1,19 +1,23 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class NFactorial {
 
-    public static int fact(int n){
+    public static long fact(long n){
         if(n==0){
             return 1;            
         }
         
-        int smallAns = fact(n-1);
+        long smallAns = fact(n-1);
         return n * smallAns;
         
     }
     public static void main(String[] args) {
-        int n = 5;
-        int ans = fact(n);
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long ans = fact(n);
         System.out.println(ans);
+        sc.close();
     }
 }
