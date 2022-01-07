780457e96de4cf67c1b4e6e9dc2cbdc644aa7db5
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2022-01-07 18:20:25 +0530

    Print Subsequences

--- Recursion2/PrintSubsequences.java
+++ Recursion2/PrintSubsequences.java
@@ -3,13 +3,10 @@ package Recursion2;
 import java.util.Scanner;
 
 public class PrintSubsequences {
-
     static int count = 0;
-
     private static void printSubsequences(String string) {
         printSubsequences(string , "");
     }
-
     private static void printSubsequences(String input, String outSoFar) {
         if(input.isEmpty()){
             count = count+1;
@@ -17,17 +14,13 @@ public class PrintSubsequences {
             System.out.println(count);
             return;
         }
-
         printSubsequences(input.substring(1), outSoFar);
         printSubsequences(input.substring(1), outSoFar + input.charAt(0));
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         printSubsequences(input);
         sc.close();
-    }
-
-    
+    } 
 }
