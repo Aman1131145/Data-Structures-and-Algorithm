0ecd5212ed174184c9052fed0d887c13d0bdc590
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2022-01-07 15:26:59 +0530

    Stair Case Problem

---
+++ Recursion2/Staircase.java
@@ -0,0 +1,27 @@
+package Recursion2;
+
+import java.util.Scanner;
+
+public class Staircase {
+
+    public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = 4;//s.nextInt();
+		System.out.println(Staircase.staircase(n));
+        s.close();
+        
+	}
+
+    private static int staircase(int n) {
+        if(n < 0){
+            return 0;
+        }
+        if(n == 0){
+            return 1;
+        }
+        int x = staircase(n - 1);
+        int y = staircase(n - 2);
+        int z = staircase(n - 3);
+        return x+y+z;
+    }    
+}
