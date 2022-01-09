ce7743a38cb9e62043fa7e800f100eedf71f60f4
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2022-01-09 13:47:43 +0530

    Return Sub Sets

--- Recursion2/ReturnSubsetOfAnArray.java
+++ Recursion2/ReturnSubsetOfAnArray.java
@@ -26,11 +26,32 @@ public class ReturnSubsetOfAnArray {
 		}
 	}
 
-    public static int[][] subsets(int[] input) {
-        if(input.length == 0){
-            return null;
+	public static int[][] subsets(int[] input){
+		return subsets(input, 0);
+	}
+
+    public static int[][] subsets(int[] input,int startIndex) {
+        if(startIndex >= input.length){
+			int[][] answer = new int[1][0];
+            return answer;
         }
-		return subsets(input);
-    }
-    
+		int[][]smallAns = subsets(input,startIndex+1);
+		int[][] ans = new int[smallAns.length*2][];
+		int k = 0;
+		for(int i = 0; i < smallAns.length; i++){
+            ans[i] = new int[smallAns[i].length];
+			for(int j = 0; j < smallAns[i].length; j++){
+				ans[i][j] = smallAns[i][j];
+			}
+			k++;
+        }
+        for(int i = 0; i < smallAns.length; i++){
+            ans[k+i] = new int[smallAns[i].length + 1];
+			ans[k+i][0] = input[startIndex];
+			for(int j = 1; j <= smallAns[i].length; j++){
+				ans[i+k][j] = smallAns[i][j-1];
+			}
+        }
+		return ans;
+	}
 }
