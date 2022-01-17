3efe0b606be3b5917b250943ede514958691f5b4
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2022-01-17 19:30:08 +0530

    Vehicle Use

---
+++ OOPs3/VehicleUse.java
@@ -0,0 +1,21 @@
+package OOPs3;
+
+public class VehicleUse {
+        
+    public static void main(String[] args) {
+        
+        // Vehicle v = new Car(5,100);
+        // v.color = "Black";
+        // v.setMaxSpeed(10);
+        // v.print();
+
+        // Car c = new Car(10,100);
+        // c.noOfGears = 10;
+        // c.color = "Black";
+        // c.setMaxSpeed(100);
+        // c.print();
+
+    }
+}
+
+
