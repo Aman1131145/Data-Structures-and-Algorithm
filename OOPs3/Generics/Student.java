31e995ab8ce79b890aacdac7362f818375368b2c
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2022-01-20 18:10:33 +0530

    Student

---
+++ OOPs3/Generics/Student.java
@@ -0,0 +1,11 @@
+package OOPs3.Generics;
+
+public class Student implements PrintInterface {
+
+    int rollNumber;
+
+    public void print(){
+        System.out.println(rollNumber);
+    }
+    
+}
