4e798a026ec83c6f9577e74939797f4a1535e091
Aman1131145 <77267079+Aman1131145@users.noreply.github.com>
2022-01-21 00:03:31 +0530

    Length of Linked Linked List

---
+++ Lists/LinkedList/LengthOfLinkedList.java
@@ -0,0 +1,78 @@
+package Lists.LinkedList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LengthOfLinkedList 
+{
+
+    public static int length(LinkedListNode<Integer> head)
+    {
+        int count = 0;
+        
+        while(head.data != -1)
+        {
+            
+            count++;
+        }
+        return count;
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+    
+    public static void print(LinkedListNode<Integer> head){
+        while(head != null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+            
+            LinkedListNode<Integer> head = takeInput();    
+            System.out.println(LengthOfLinkedList.length(head));
+            System.out.println();
+
+            t -= 1;
+        }
+    }
+    
+}
+class LinkedListNode<T> 
+{
+    T data;
+    LinkedListNode<T> next;
+    
+    public LinkedListNode(T data) 
+    {
+        this.data = data;
+    }
+}
