d99f7221419fa214ad9f00b17cb9f7ce4a1b9fd3
Aman <mishraaman113114@gmail.com>
2022-01-25 18:58:13 +0530

    Print I th Node

---
+++ Lists/SinglyLinkedList/PrintIthNode.java
@@ -0,0 +1,73 @@
+package Lists.SinglyLinkedList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+    public LinkedListNode(T data, LinkedListNode<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+/**
+ * PrintIthNode
+ */
+public class PrintIthNode {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+        return head;
+    }
+    
+    public static void print(LinkedListNode<Integer> head){
+        while(head != null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t > 0) {   
+            LinkedListNode<Integer> head = takeInput(); 
+            int i = Integer.parseInt(br.readLine().trim());   
+            PrintIthNode.printIthNode(head, i);
+
+            t -= 1;
+        }
+    }
+
+    public static void printIthNode(LinkedListNode<Integer> head, int i) {
+        for(int j=0; j<i && head != null; j++) {
+            head = head.next;
+        }
+        if(head != null) {   
+            System.out.print(head.data);
+        }
+    }
+}
