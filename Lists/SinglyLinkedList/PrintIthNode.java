55d49fb22957740fe8f7a2117419552779145ad1
Aman <mishraaman113114@gmail.com>
2022-01-26 21:49:27 +0530

    Print the ith node in a linked list

--- Lists/SinglyLinkedList/PrintIthNode.java
+++ Lists/SinglyLinkedList/PrintIthNode.java
@@ -4,17 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class LinkedListNode<T> {
-    T data;
-    LinkedListNode<T> next;
-    public LinkedListNode(T data, LinkedListNode<T> next) {
-        this.data = data;
-        this.next = next;
-    }
-    public LinkedListNode(T data) {
-        this.data = data;
-    }
-}
 /**
  * PrintIthNode
  */
