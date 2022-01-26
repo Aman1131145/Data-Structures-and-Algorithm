aeca47c7027dcce340a438d0cef86f3c8cb997a4
Aman <mishraaman113114@gmail.com>
2022-01-26 21:49:00 +0530

    Mid Point of Linked List

---
+++ Lists/SinglyLinkedList/MidPointOfLinkedList.java
@@ -0,0 +1,83 @@
+package Lists.SinglyLinkedList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+    
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+public class MidPointOfLinkedList {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+    
+    public static void print(LinkedListNode<Integer> head){
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        
+        System.out.println();
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+
+            LinkedListNode<Integer> head = takeInput(); 
+
+            LinkedListNode<Integer> mid = MidPointOfLinkedList.midPoint(head);
+            
+            if (mid != null) {
+                System.out.println(mid.data);
+            }
+
+            t -= 1;
+        }
+
+    }
+
+    public static LinkedListNode<Integer> midPoint(LinkedListNode<Integer> head) {
+        
+        LinkedListNode<Integer> slow = head,fast = head;
+        if(head == null || head.next == null){
+            return head;
+        }
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+    
+}
