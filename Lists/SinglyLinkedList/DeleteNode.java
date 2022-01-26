5b4140e41b5b453a4df0bb44f72db5fa91cef135
Aman <mishraaman113114@gmail.com>
2022-01-27 00:04:16 +0530

    Delete Node

--- Lists/SinglyLinkedList/DeleteNode.java
+++ Lists/SinglyLinkedList/DeleteNode.java
@@ -4,19 +4,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+class Delete<T> {
+    T data;
+    Delete<T> next;
+    
+    public Delete(T data) {
+        this.data = data;
+    }
+}
+
 public class DeleteNode {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
-    public static LinkedListNode<Integer> takeInput() throws IOException {
-        LinkedListNode<Integer> head = null, tail = null;
+    public static Delete<Integer> takeInput() throws IOException {
+        Delete<Integer> head = null, tail = null;
 
         String[] datas = br.readLine().trim().split("\\s");
 
         int i = 0;
         while(i < datas.length && !datas[i].equals("-1")) {
             int data = Integer.parseInt(datas[i]);
-            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            Delete<Integer> newNode = new Delete<Integer>(data);
             if(head == null) {
                 head = newNode;
                 tail = newNode;
@@ -31,7 +40,7 @@ public class DeleteNode {
         return head;
     }
     
-    public static void print(LinkedListNode<Integer> head){
+    public static void print(Delete<Integer> head){
         while(head != null) {
             System.out.print(head.data + " ");
             head = head.next;
@@ -44,7 +53,7 @@ public class DeleteNode {
         int t = Integer.parseInt(br.readLine().trim());
 
         while (t > 0) {
-            LinkedListNode<Integer> head = takeInput(); 
+            Delete<Integer> head = takeInput(); 
 
             int pos = Integer.parseInt(br.readLine().trim());
             head = DeleteNode.deleteNode(head, pos);
@@ -54,8 +63,8 @@ public class DeleteNode {
         }
     }
 
-    public static LinkedListNode<Integer> deleteNode(LinkedListNode<Integer> head, int pos) {
-        LinkedListNode<Integer> temp = head;
+    public static Delete<Integer> deleteNode(Delete<Integer> head, int pos) {
+        Delete<Integer> temp = head;
         if(head == null){
             return head;
         }
