6f4c221ced4a230182858816c4b267efb3233bbb
Aman <mishraaman113114@gmail.com>
2022-01-26 22:48:13 +0530

    Length of linked list

--- Lists/SinglyLinkedList/LengthOfLinkedList.java
+++ Lists/SinglyLinkedList/LengthOfLinkedList.java
@@ -86,13 +86,3 @@ public class LengthOfLinkedList
     }
     
 }
-class LinkedListNode<T> 
-{
-    T data;
-    LinkedListNode<T> next;
-    
-    public LinkedListNode(T data) 
-    {
-        this.data = data;
-    }
-}
