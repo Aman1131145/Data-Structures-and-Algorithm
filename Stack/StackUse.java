b9296479f8c2b968dfbca75da6490bf4eeb42714
Aman <mishraaman113114@gmail.com>
2022-02-02 22:49:31 +0530

    Stack Use

--- Stack/StackUse.java
+++ Stack/StackUse.java
@@ -3,7 +3,7 @@ package Stack;
 public class StackUse {
 
     public static void main(String[] args) throws StackFullException {
-        StackUsingArray stack = new StackUsingArray(3);
+        StackUsingLinkedList<Integer> stack = new StackUsingLinkedList<>();
         for(int i = 1; i <= 5; i++){
             stack.push(i);
         }
