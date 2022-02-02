5976ba6420830eb998dc1bd386befcc193dd004f
Aman <mishraaman113114@gmail.com>
2022-02-02 23:26:52 +0530

    Stack Collection

---
+++ Stack/StackCollection.java
@@ -0,0 +1,24 @@
+package Stack;
+
+import java.util.Stack;
+
+/**
+ * StackCollection
+ */
+public class StackCollection {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        int arr[] = {5,2,8,1};
+        for(int i = 0; i < arr.length; i++){
+            stack.push(arr[i]);
+        }    
+
+        while(!stack.isEmpty()){
+            // int temp = stack.peek();
+            // System.out.println(temp);
+            System.out.println(stack.peek());
+            // System.out.println(stack.pop());
+        }
+    }
+}
