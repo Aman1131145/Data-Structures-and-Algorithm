989124bc850c996054733311ba794fc97de74fda
Aman <mishraaman113114@gmail.com>
2022-02-02 22:33:36 +0530

    Stack using linked lIst

---
+++ Stack/StackUsingLinkedList.java
@@ -0,0 +1,18 @@
+package Stack;
+
+class Node {
+	int data;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+
+public class StackUsingLinkedList {
+
+
+    
+}
