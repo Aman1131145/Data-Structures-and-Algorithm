e517bf53a1cf05a6c2dd202c6267f4c5fa59cf29
Aman <mishraaman113114@gmail.com>
2022-02-02 22:49:23 +0530

    Stack Using Linked List

--- Stack/StackUsingLinkedList.java
+++ Stack/StackUsingLinkedList.java
@@ -1,18 +1,46 @@
 package Stack;
 
-class Node {
-	int data;
-	Node next;
+public class StackUsingLinkedList<T> {
 
-	public Node(int data) {
-		this.data = data;
-		this.next = null;
+	private StackNode<T> head;
+	private int size;
+
+	public StackUsingLinkedList(){
+		head = null;
+		size = 0;
 	}
-}
 
+	int size(){
+		return size;
+	}
 
-public class StackUsingLinkedList {
+	boolean isEmpty(){
+		return size() == 0; // head == null;
+	}
 
+	T top() throws StackEmptyException{
+		if(size() == 0){
+			// StackEmptyException e = new StackEmptyException();
+			// throw e;
+			throw new StackEmptyException();
+		}
+		return head.data;
+	}
 
+	void push(T element){
+		StackNode<T> newNode = new StackNode<T>(element);
+		newNode.next = head;
+		head = newNode;
+		size++;
+	}
+
+	T pop() throws StackEmptyException{
+		if(size() == 0){
+			throw new StackEmptyException();
+		}
+		T tempdata = head.data;
+		head = head.next;
+		return tempdata;
+	}
     
 }
