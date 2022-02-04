38e7edd8b33a4a6e07929394b1205b3f39633bf3
Aman <mishraaman113114@gmail.com>
2022-02-04 14:46:41 +0530

    Balanced Parenthesis

--- Stack/BalanceParanthesis.java
+++ Stack/BalanceParanthesis.java
@@ -14,58 +14,24 @@ public class BalanceParanthesis {
         System.out.println(BalanceParanthesis.isBalanced(expression));
     }
 
-    // public static boolean isBalanced(String expression) {
-    //     //Your code goes here
-    //     Stack<String> stack = new Stack<>();
-    //     String p = null;
-    //     for(int i = 0; i < expression.length(); i++){
-    //         String c = expression.substring(i,i+1);
-    //         if((c.contains("}") || c.contains(")") || c.contains("]")) && stack.size() == 0){
-    //             return false;
-    //         }
-    //         if(c.contains("{") || c.contains("(") || c.contains("[")){
-    //             stack.push(c);
-    //             p = stack.peek();
-    //         }
-    //         if((c.contains(")") && p.contains("(")) || (c.contains("}") && p.contains("{")) || c.contains("]") && p.contains("[")){
-	// 			stack.pop();
-    //         }         
-    //     }
-    //     return stack.isEmpty();
-    // }
-
     public static boolean isBalanced(String expression) {
         //Your code goes here
-        Stack<String> stack=new Stack<>();
-        String temp=expression;
-        int i=0;
-        while(i<temp.length()){
-            if(temp.charAt(i)=='[' || temp.charAt(i)=='{' || temp.charAt(i)=='(' ){
-                stack.push(temp.charAt(i));
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < expression.length(); i++){
+            if(expression.charAt(i)=='('){
+                stack.push(expression.charAt(i));
             }
-            // if get closing bracket
-            else if(temp.charAt(i)==']' || temp.charAt(i)=='}' || temp.charAt(i)==')'){
-                if(stack.isEmpty())
+            if(expression.charAt(i)==')'){
+                if(stack.size() == 0){
                     return false;
-                else if(!matchPair((char)stack.pop(),temp.charAt(i)))
-                    return false;
-            }
-            i++;
+                }
+                else{
+                    if(stack.peek()=='('){
+                        stack.pop();
+                    }
+                }
+            }                    
         }
-        if(stack.isEmpty())
-            return true;
-        else 
-            return false;
-
-	}
-    public static boolean matchPair(char par1,char par2){
-        if(par1=='{' && par2=='}')
-            return true;
-        else if(par1=='(' && par2==')')
-            return true;
-        else if(par1=='[' && par2==']')
-            return true;
-        else
-            return false;
+        return stack.isEmpty();
     }
 }
