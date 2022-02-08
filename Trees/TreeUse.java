2d4e92f7d720eec0b0de270ec7978638aca1f6be
Aman <mishraaman113114@gmail.com>
2022-02-08 13:23:15 +0530

    Tree Use

--- Trees/TreeUse.java
+++ Trees/TreeUse.java
@@ -118,29 +118,30 @@ public class TreeUse {
     // }
     
     private static TreeNode<Integer> takeInputLevelWise(){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter root data : ");
-        int rootData = s.nextInt();
-        Queue<TreeNode<Integer>> pendingNodes = new LinkedList<>();
-        TreeNode<Integer> root = new TreeNode<>(rootData);
-        pendingNodes.add(root);
-        while(!pendingNodes.isEmpty()){
-            try {
-                TreeNode<Integer> frontNode = pendingNodes.poll();
-                System.out.println("Enter num of children of " + frontNode.data + " : ");
-                int numChildren = s.nextInt();
-                for (int i = 0; i < numChildren; i++) {
-                    System.out.println("Enter " + (i + 1) + "th child of " + frontNode.data + " : ");
-                    int child = s.nextInt();
-                    TreeNode<Integer> childNode = new TreeNode<Integer>(child);
-                    frontNode.children.add(childNode);
-                    pendingNodes.add(childNode);
-                    
-                }
-            } catch (Exception e) {
-                return null;
-            }
-        }
-        return root;
+        try (Scanner s = new Scanner(System.in)) {
+			System.out.println("Enter root data : ");
+			int rootData = s.nextInt();
+			Queue<TreeNode<Integer>> pendingNodes = new LinkedList<>();
+			TreeNode<Integer> root = new TreeNode<>(rootData);
+			pendingNodes.add(root);
+			while(!pendingNodes.isEmpty()){
+			    try {
+			        TreeNode<Integer> frontNode = pendingNodes.poll();
+			        System.out.println("Enter num of children of " + frontNode.data + " : ");
+			        int numChildren = s.nextInt();
+			        for (int i = 0; i < numChildren; i++) {
+			            System.out.println("Enter " + (i + 1) + "th child of " + frontNode.data + " : ");
+			            int child = s.nextInt();
+			            TreeNode<Integer> childNode = new TreeNode<Integer>(child);
+			            frontNode.children.add(childNode);
+			            pendingNodes.add(childNode);
+			            
+			        }
+			    } catch (Exception e) {
+			        return null;
+			    }
+			}
+			return root;
+		}
     }
 }
