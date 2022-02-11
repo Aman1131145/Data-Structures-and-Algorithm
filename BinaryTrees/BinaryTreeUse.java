cbfc35ad9079d94db07862b3b0326a8860c820cb
Aman <mishraaman113114@gmail.com>
2022-02-11 22:07:37 +0530

    Binary Tree Use

--- BinaryTrees/BinaryTreeUse.java
+++ BinaryTrees/BinaryTreeUse.java
@@ -7,7 +7,8 @@ public class BinaryTreeUse {
         // Scanner s = new Scanner(System.in);
         // BinaryTreeNode<Integer> root = takeInput(s);
         BinaryTreeNode<Integer> root = takeInputLevelWise();
-        printTree(root);
+        printLevelWise(root);
+        System.out.println("Number of Nodes in this tree : " + countNodes(root));
         // s.close();
         // BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
         // BinaryTreeNode<Integer> node1 = new BinaryTreeNode<Integer>(2);
@@ -15,68 +16,111 @@ public class BinaryTreeUse {
         // BinaryTreeNode<Integer> node2 = new BinaryTreeNode<Integer>(3);
         // root.right = node2;
     }
-    private static BinaryTreeNode<Integer> takeInput(Scanner s) {
-        int rootData;
-        System.out.println("Enter root data");
-        rootData = s.nextInt();
-        if(rootData == -1){
-            return null;
-        }
-        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
-        root.left = takeInput(s);
-        root.right = takeInput(s);
-        return root;
-    }
-
-    public static void printTree(BinaryTreeNode<Integer> root){
-        if(root == null){
-            return;
-        }
-        String toBePrinted = root.data + ":";
-        if(root.left != null){
-            toBePrinted += "L:" + root.left.data + ",";
-        }
-        if(root.right != null){
-            toBePrinted += "R:" + root.right.data;
-        }
-        System.out.println(toBePrinted);
-        printTree(root.left);
-        printTree(root.right);
-    }
-
+    
     public static BinaryTreeNode<Integer> takeInputLevelWise(){
-        Scanner s = new Scanner(System.in);
-        Queue<BinaryTreeNode<Integer>> queue = new LinkedList<>();
-        System.out.println("Enter root data");
-        int rootData = s.nextInt();
-        if(rootData == -1){
-            return null;
-        }
-        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
-        queue.add(root);
-
-        while(!queue.isEmpty()){
-            BinaryTreeNode<Integer> front;
-            try{
-                front = queue.remove();
-            } catch (Exception e) {
+        try (Scanner s = new Scanner(System.in)) {
+            Queue<BinaryTreeNode<Integer>> queue = new LinkedList<>();
+            System.out.println("Enter root data");
+            int rootData = s.nextInt();
+            if(rootData == -1){
                 return null;
             }
-            System.out.println("Enter left child of " + front.data);
-            int leftChild = s.nextInt();
-            if(leftChild != -1){
-                BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(leftChild);
-                queue.add(child);
-                front.left = child;
+            BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+            queue.add(root);
+            
+            while(!queue.isEmpty()){
+                BinaryTreeNode<Integer> front;
+                try{
+                    front = queue.remove();
+                } catch (Exception e) {
+                    return null;
+                }
+                System.out.println("Enter left child of " + front.data);
+                int leftChild = s.nextInt();
+                if(leftChild != -1){
+                    BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(leftChild);
+                    queue.add(child);
+                    front.left = child;
+                }
+                System.out.println("Enter right child of " + front.data);
+                int rightChild = s.nextInt();
+                if(rightChild != -1){
+                    BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(rightChild);
+                    queue.add(child);
+                    front.right = child;
+                }
             }
-            System.out.println("Enter right child of " + front.data);
-            int rightChild = s.nextInt();
-            if(rightChild != -1){
-                BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(rightChild);
-                queue.add(child);
-                front.right = child;
-            }
+            return root;
         }
-        return root;
     }
+
+    public static void printLevelWise(BinaryTreeNode<Integer> root) {
+		//Your code goes here
+        Queue<BinaryTreeNode<Integer>> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            BinaryTreeNode<Integer> frontNode = queue.remove();
+            if(frontNode == null){
+                if(queue.isEmpty()){
+                    break;
+                }
+                System.out.println();
+            }
+            else{
+                System.out.print(frontNode.data + ":");
+                if(frontNode.left != null){
+                    queue.add(frontNode.left);
+                    System.out.print("L:" + frontNode.left.data + ",");
+                }else{
+                    System.out.print("L:-1,");
+                }
+                if(frontNode.right != null){
+                    queue.add(frontNode.right);
+                    System.out.print("R:" + frontNode.right.data);
+                }else{
+                    System.out.print("R:-1");
+                }
+            }
+            System.out.println();
+        }
+	}
+    
+    public static int countNodes(BinaryTreeNode<Integer> root){
+        if(root == null){
+            return 0;
+        }
+        int ans = 1;
+        ans += countNodes(root.left);
+        ans += countNodes(root.right);
+        return ans;
+    }
+    // private static BinaryTreeNode<Integer> takeInput(Scanner s) {
+    //     int rootData;
+    //     System.out.println("Enter root data");
+    //     rootData = s.nextInt();
+    //     if(rootData == -1){
+    //         return null;
+    //     }
+    //     BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+    //     root.left = takeInput(s);
+    //     root.right = takeInput(s);
+    //     return root;
+    // }
+
+    // public static void printTree(BinaryTreeNode<Integer> root){
+    //     if(root == null){
+    //         return;
+    //     }
+    //     String toBePrinted = root.data + ":";
+    //     if(root.left != null){
+    //         toBePrinted += "L:" + root.left.data + ",";
+    //     }
+    //     if(root.right != null){
+    //         toBePrinted += "R:" + root.right.data;
+    //     }
+    //     System.out.println(toBePrinted);
+    //     printTree(root.left);
+    //     printTree(root.right);
+    // }
 }
