fdb12a22f2d9a15c32b26b0d5db2b2d7ea4252a0
Aman Mishra <mishraaman113114@gmail.com>
2022-02-23 22:10:40 +0530

    Pair Sum

--- BinarySearchTrees/PairSum.java
+++ BinarySearchTrees/PairSum.java
@@ -64,37 +64,49 @@ public class PairSum {
     static ArrayList<Integer> arr = new ArrayList<>();
     static String s = "";
 
-	static void pairSum(BinaryTreeNode<Integer> root, int sum) {
-
-		/* Your class should be named Solution
-		 * Don't write main().
-		 * Don't read input, it is passed as function argument.
-		 * Return output and don't print it.
-	 	 * Taking input and printing output is handled automatically.
-        */
-        Queue<BinaryTreeNode<Integer>> queue = new LinkedList<>();
-        if(root == null){
+	public static void pairSum(BinaryTreeNode<Integer> root, int sum) {
+        if(root==null)
             return;
+        int[] arr=arrayInsertion(root);
+        Arrays.sort(arr);
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            if(arr[i]+arr[j]==sum){
+                System.out.println(arr[i]+" "+arr[j]);
+                i++;
+                j--;}
+            else if(arr[i]+arr[j]<sum)
+                i++;
+            else 
+                j--;
+
         }
-        queue.add(root);
-        arr.add(root.data);
-        while(!queue.isEmpty()){
-            BinaryTreeNode<Integer> front = queue.remove();
-            if(front.left != null){
-                queue.add(front.left);
-                arr.add(front.left.data);
-            }
-            if(front.right != null){
-                queue.add(front.right);
-                arr.add(front.right.data);
-            }
+    }
+    private static int[] arrayInsertion(BinaryTreeNode<Integer> root){
+
+        if(root==null){
+            int[] arr=new int[0];
+            return arr;}
+        int firstData=root.data;
+
+            int[] jrr= arrayInsertion(root.left);
+
+        int[] krr= arrayInsertion(root.right);
+        int[] finalArray=new int[1+jrr.length+krr.length];
+        int k=0;
+        finalArray[k]=firstData;
+        k++;
+        for(int i=0;i<jrr.length;i++)
+        {  finalArray[k]=jrr[i];
+         k++;
         }
-        
-        for(int i = 0 ; i < arr.size()-1; i++){
-            int c = sum - arr.get(i);
-            if(arr.contains(c)){
-                System.out.println(arr.get(i) + " " + c);
-            }
+        for(int i=0;i<krr.length;i++)
+        {
+            finalArray[k]=krr[i];
+            k++;
         }
-	}
+        return finalArray;
+
+    }
 }
