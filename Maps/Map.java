1c4eabf174510cffc85e3a1d54532d5c90106ae8
Aman Mishra <mishraaman113114@gmail.com>
2022-03-08 15:05:18 +0530

    Map

--- Maps/Map.java
+++ Maps/Map.java
@@ -8,9 +8,9 @@ public class Map<K,V> {
     int numBuckets;
 
     public Map(){
-        numBuckets = 20;
+        numBuckets = 5;
         buckets = new ArrayList<>();
-        for(int i = 0;i < 20; i++){
+        for(int i = 0;i < 5; i++){
             buckets.add(null);
         }
     }
@@ -21,8 +21,8 @@ public class Map<K,V> {
     }
 
     public void insert(K key, V value){
-        int BucketIndex = getBucketIndex(key);
-        MapNode<K,V> head = buckets.get(BucketIndex);
+        int bucketIndex = getBucketIndex(key);
+        MapNode<K,V> head = buckets.get(bucketIndex);
         while(head != null){
             if(head.key.equals(key)){
                 head.value = value;
@@ -30,15 +30,45 @@ public class Map<K,V> {
             }
             head = head.next;
         }
-        head = buckets.get(BucketIndex);
+        head = buckets.get(bucketIndex);
         MapNode<K,V> newElementNode = new MapNode<>(key, value);
-        newElementNode = head;
-        buckets.set(BucketIndex, newElementNode);
+        size++;
+        newElementNode.next = head;
+        buckets.set(bucketIndex, newElementNode);
+        double loadFactor = (1.0 * size)/numBuckets;
+        if(loadFactor > 0.7){
+            rehash();
+        }
+    }
+    
+    private void rehash(){
+        System.out.println("Rehashing : Buckets " + numBuckets + " size = " + size);
+        ArrayList<MapNode<K,V>> temp = buckets;
+        buckets = new ArrayList<>();
+        for (int i = 0; i < 2*numBuckets; i++) {
+            buckets.add(null);
+        }
+        size = 0;
+        numBuckets *= 2;
+
+        for (int j = 0; j < temp.size(); j++) {
+            MapNode<K,V> head = temp.get(j);
+            while(head != null){
+                K key = head.key;
+                V value = head.value;
+                insert(key,value);
+                head = head.next;
+            }
+        }
+    }
+
+    public double loadFactor(){
+        return (1.0 * size)/numBuckets;
     }
     
     public V getValue(K key){
-        int BucketIndex = getBucketIndex(key);
-        MapNode<K,V> head = buckets.get(BucketIndex);
+        int bucketIndex = getBucketIndex(key);
+        MapNode<K,V> head = buckets.get(bucketIndex);
         while(head != null){
             if(head.key.equals(key)){
                 return head.value;
@@ -49,13 +79,14 @@ public class Map<K,V> {
     }
     
     public V removeKey(K key){
-        int BucketIndex = getBucketIndex(key);
-        MapNode<K,V> head = buckets.get(BucketIndex);
+        int bucketIndex = getBucketIndex(key);
+        MapNode<K,V> head = buckets.get(bucketIndex);
         MapNode<K,V> prev = null;
         while(head != null){
             if(head.key.equals(key)){
+                size--;
                 if(prev == null){
-                    buckets.set(BucketIndex,head.next); 
+                    buckets.set(bucketIndex,head.next); 
                 }else{
                     prev.next = head.next;
                 }
