34edff4ebd7a46e4b06ed4b6d5f882a9d146b37a
Aman Mishra <mishraaman113114@gmail.com>
2022-03-08 15:05:38 +0530

    Our Map Use

---
+++ Maps/OurMapUse.java
@@ -0,0 +1,18 @@
+package Maps;
+
+public class OurMapUse {
+    public static void main(String[] args) {
+        Map<String,Integer> map = new Map<>();
+        for (int i = 0; i < 20; i++) {
+            map.insert("abc" + i, 1 + i);
+            System.out.println("i = " + i + " if = " + map.loadFactor());
+        }
+
+        map.removeKey("abc3");
+        map.removeKey("abc7");
+        
+        for(int i = 0; i < 20; i++){
+            System.out.println("abc" + i + "i" + map.getValue("abc + i"));
+        }
+    }
+}
