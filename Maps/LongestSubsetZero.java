a5277386da95817f576043a2578a482d42c2c0d8
Aman Mishra <mishraaman113114@gmail.com>
2022-03-09 23:18:21 +0530

    Longest Subset Zero

---
+++ Maps/LongestSubsetZero.java
@@ -0,0 +1,51 @@
+package Maps;
+
+import java.io.*;
+import java.util.*;
+
+public class LongestSubsetZero {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int arr[] = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		System.out.println(LongestSubsetZero.lengthOfLongestSubsetWithZeroSum(arr));
+	}
+
+    public static int lengthOfLongestSubsetWithZeroSum(int arr[]) {
+		// Write your code here
+        
+        if(arr.length==0)
+            return 0;
+        HashMap<Integer,Integer> h=new HashMap<>();
+        int sum=0;
+        int maxlength=0;
+        int maxlength1=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum=sum+arr[i];
+            if(h.containsKey(sum)){
+                int k=h.get(sum);
+                int l=i-k;
+                maxlength1=l;}
+             // if(!h.containsKey(sum))
+            else
+            h.put(sum,i);
+            if(sum==0)
+            {
+                maxlength1=i+1;
+            }
+           
+            if(maxlength1>maxlength)
+                maxlength=maxlength1;
+        }
+		return maxlength;
+	}
+    
+}
