ea692e93c01cd83541c857986f623aa67084eddc
Aman Mishra <mishraaman113114@gmail.com>
2022-03-28 20:38:12 +0530

    Trie

---
+++ Tries/Trie.java
@@ -0,0 +1,58 @@
+package Tries;
+
+class TrieNode{
+    char data;
+    boolean isTerminating;
+    TrieNode children[];
+
+    public TrieNode(char data){
+        this.data = data;
+        isTerminating = false;
+        children = new TrieNode[26];
+    }
+}
+
+/**
+ * Trie
+ */
+public class Trie {
+
+    private TrieNode root;
+
+    public Trie(){
+        root = new TrieNode('\0');
+    }
+
+    public boolean search(String word){
+        return search(root,word);
+    }
+    private boolean search(TrieNode root,String word){
+        if(word.length()==0){
+            return root.isTerminating;
+        }
+        int childIndex=word.charAt(0)-'a';
+        TrieNode child=root.children[childIndex];
+        if(child==null){
+            return false;
+        }
+        return search(child,word.substring(1));
+    }
+
+    private void add(TrieNode root, String word){
+        if(word.length() == 0){
+            root.isTerminating = true;
+            return;
+        }
+        int childIndex = word.charAt(0)-'a';
+        TrieNode child = root.children[childIndex];
+        if(child == null){
+            child = new TrieNode(word.charAt(0));
+            root.children[childIndex] = child;
+        }
+        add(child,word.substring(1));
+    }
+
+    public void add (String word){
+        add(root,word);
+    }
+}
