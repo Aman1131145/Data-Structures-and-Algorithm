9e0ac9847c637ed74d3f7315de275b9aec468eec
Aman Mishra <mishraaman113114@gmail.com>
2022-03-29 19:12:46 +0530

    Trie use

---
+++ Tries/TrieUse.java
@@ -0,0 +1,15 @@
+package Tries;
+
+/**
+ * TrieUse
+ */
+public class TrieUse {
+
+    public static void main(String[] args) {
+        Trie t = new Trie();
+        t.add("this");
+        t.add("that");
+        t.add("news");
+        System.out.println(t.search("news"));
+    }
+}
