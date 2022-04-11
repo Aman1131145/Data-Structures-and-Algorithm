5bcae9e1132e165bd990db6c6a2580280da627a1
Aman Mishra <mishraaman113114@gmail.com>
2022-04-11 21:55:35 +0530

    Largest common subsequence

--- DP2/LCS.java
+++ DP2/LCS.java
@@ -54,8 +54,30 @@ public class LCS {
         return storage[m][n];
     }
 
+    public static int lcsDP(String s, String t){
+        int m = s.length();
+        int n = t.length();
+        int storage[][] = new int [m+1][n+1];
+        for (int i = 0; i < m; i++) {
+            storage[i][0] = 0;
+        }
+        for(int i = 0; i <= n; i++){
+            storage[0][i] = 0;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(s.charAt(m-i) == t.charAt(n-j)){
+                    storage[i][j] = 1 + storage[i-1][j-1];
+                }else{
+                    storage[i][j] = Math.max(storage[i][j-1], storage[i-1][j]);
+                }
+            }
+        }
+        return storage[m][n];
+    }
     public static void main(String[] args) {
-        System.out.println(lcsM("helloauntyji", "whatthehellisthis"));
-        System.out.println(lcsR("helloauntyji", "whatthehellisthis"));
+        System.out.println(lcsDP("hello my name is aman", "what is your name"));
+        System.out.println(lcsM("hello my name is aman", "what is your name"));
+        System.out.println(lcsR("hello my name is aman", "what is your name"));
     }
 }
