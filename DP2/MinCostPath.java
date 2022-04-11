e8c2d95b0586c63ceb5710e829b627801e598369
Aman Mishra <mishraaman113114@gmail.com>
2022-04-11 15:06:19 +0530

    Minimum cost path

--- DP2/MinCostPath.java
+++ DP2/MinCostPath.java
@@ -59,10 +59,33 @@ public class MinCostPath {
         return storage[i][j];
     }
 
+    public static int minCostPathDP(int arr[][]){
+        int m = arr.length;
+        int n = arr[0].length;
+        int storage[][] = new int[m][n];
+        storage[m-1][n-1] = arr[m-1][n-1];
+        // last row
+        for(int j = n-2; j >= 0; j--){
+            storage[n-1][j] = storage[m-1][j+1] + arr[m-1][j];
+        }
+        // last coloumn
+        for(int i = m-2; i >= 0; i--){
+            storage[i][n-1] = storage[i+1][n-1] + arr[i][n-1];
+        }
+
+        for(int i = m-2; i >= 0 ; i--){
+            for(int j = n-2; j >= 0; j--){
+                storage[i][j] =arr[i][j] + Math.min(storage[i][j+1], Math.min(storage[i+1][j+1], storage[i+1][j]));
+            }
+        }
+        return storage[0][0];
+    }
+
     public static void main(String args[]){
         int arr[][] = {{1,1,1}, {4,5,2}, {7,8,9}};
         System.out.println(minCostPathR(arr));
         System.out.println(minCostPathM(arr));
+        System.out.println(minCostPathDP(arr));
     }
     
 }
