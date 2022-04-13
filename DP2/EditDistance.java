a2b2fe4efaca2f26af531115715b6e95b1f2e84e
Aman Mishra <mishraaman113114@gmail.com>
2022-04-13 17:07:15 +0530

    Edit distance

--- DP2/EditDistance.java
+++ DP2/EditDistance.java
@@ -22,9 +22,75 @@ public class EditDistance {
             return 1+Math.min(op1, Math.min(op2,op3));            
         }
     }
+
+    public static int editDistanceM(String s, String t){
+        int m = s.length();
+        int n = t.length();
+        int storage[][] = new int[m+1][n+1];
+        for(int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                storage[i][j] = -1;
+            }
+        }
+        return editDistanceM(s, t, storage);
+    }
+
+    public static int editDistanceM(String s, String t, int[][] storage){
+        int m = s.length();
+        int n = t.length();
+        if(storage[m][n] != -1){
+            return storage[m][n];
+        }
+        if(m == 0){
+            storage[m][n] = n;
+            return storage[m][n];
+        }
+        if(n == 0){
+            storage[m][n] = m;
+            return storage[m][n];
+        }
+        if(s.charAt(0) == t.charAt(0)){
+            return editDistanceM(s.substring(1), t.substring(1), storage);
+        }else{
+            //insert
+            int op1 = editDistanceM(s, t.substring(1), storage);
+            //delete
+            int op2 = editDistanceM(s.substring(1), t, storage);
+            //substitute
+            int op3 = editDistanceM(s.substring(1), t.substring(1), storage);
+
+            return 1+Math.min(op1, Math.min(op2,op3));            
+        }
+
+    }
+
+    public static int editdistanceDP(String s, String t){
+        int m = s.length();
+        int n = t.length();
+        int storage[][] = new int[m+1][n+1];
+        for(int i = 0; i <= m; i++){
+            storage[i][0] = i;
+        }
+        for(int j = 0; j <= n; j++){
+            storage[0][j] = j;
+        }
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(s.charAt(m-i) == t.charAt(n-j)){
+                    storage[i][j] = storage[i-1][j-1];
+                }else{
+                    storage[i][j] = 1 + Math.min(storage[i][j-1],
+                                                 Math.min(storage[i-1][j], storage[i-1][j-1]));
+                }
+            }
+        }
+        return storage[m][n];
+    }
     public static void main(String[] args) {
-        String s = "adef";
-        String t = "gbde";
+        String s = "pagalHaikya";
+        String t = "nahiBheNalle";
+        System.out.println(editdistanceDP(s, t));
+        System.out.println(editDistanceM(s, t));
         System.out.println(editDistanceR(s, t));
     }
 }
