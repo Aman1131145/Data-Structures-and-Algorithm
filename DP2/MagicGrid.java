1bf70004a8cf9aff2fd527aea6567db354cd33b3
Aman Mishra <mishraaman113114@gmail.com>
2022-04-22 22:35:54 +0530

    Magic grid

--- DP2/MagicGrid.java
+++ DP2/MagicGrid.java
@@ -42,31 +42,27 @@ public class MagicGrid {
 	}
 
     public static int getMinimumStrength(int[][] grid) {
-        return minInitialPoints(grid,grid.length,grid[0].length);
-    }
-    
-    static int minInitialPoints(int points[][],int R,int C)
-    {
-        // dp[i][j] represents the minimum initial points player
+        // dp[i][j] represents the minimum initial grid player
         // should have so that when starts with cell(i, j) successfully
         // reaches the destination cell(m-1, n-1)
-        int dp[][] = new int[R][C];
-        int m = R, n = C;
+        int m = grid.length, n = grid[0].length;
+        
+        int dp[][] = new int[m][n];
        
         // Base case
-        if(points[m-1][n-1] > 0){
+        if(grid[m-1][n-1] > 0){
             dp[m-1][n-1] = 1;
         }else{
-            dp[m-1][n-1] = Math.abs(points[m-1][n-1]) + 1;
+            dp[m-1][n-1] = Math.abs(grid[m-1][n-1]) + 1;
         }
        
         // Fill last row and last column as base to fill
         // entire table
         for (int i = m-2; i >= 0; i--){
-             dp[i][n-1] = Math.max(dp[i+1][n-1] - points[i][n-1], 1);
+             dp[i][n-1] = Math.max(dp[i+1][n-1] - grid[i][n-1], 1);
         }
         for (int j = n-2; j >= 0; j--){
-             dp[m-1][j] = Math.max(dp[m-1][j+1] - points[m-1][j], 1);
+             dp[m-1][j] = Math.max(dp[m-1][j+1] - grid[m-1][j], 1);
         }
        
         // fill the table in bottom-up fashion
@@ -74,11 +70,10 @@ public class MagicGrid {
         {
             for (int j=n-2; j>=0; j--)
             {
-                int min_points_on_exit = Math.min(dp[i+1][j], dp[i][j+1]);
-                dp[i][j] = Math.max(min_points_on_exit - points[i][j], 1);
+                int min_grid_on_exit = Math.min(dp[i+1][j], dp[i][j+1]);
+                dp[i][j] = Math.max(min_grid_on_exit - grid[i][j], 1);
             }
          }
-       
          return dp[0][0];
     }
 }
